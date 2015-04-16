174be877a9fe0c18bd98ecc4f79f232e98d0329e
Keith <miece16@hotmail.com>
2015-04-16 19:19:55 +0100

    comments

--- src/com/example/myfirstapp/AddItemActivity.java
+++ src/com/example/myfirstapp/AddItemActivity.java
@@ -1,19 +1,14 @@
 package com.example.myfirstapp;
 
-
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
-
 import java.net.URL;
-
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.entity.BufferedHttpEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
-
-
 import com.parse.GetCallback;
 import com.parse.ParseException;
 import com.parse.ParseFile;
@@ -22,8 +17,6 @@ import com.parse.ParseQuery;
 import com.parse.ParseUser;
 import com.parse.SaveCallback;
 import com.squareup.picasso.Picasso;
-
-import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.Intent;
@@ -37,17 +30,14 @@ import android.view.View;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.FrameLayout;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.TextView;
 import android.widget.Toast;
 
 public class AddItemActivity extends BaseActivity  {
 	
-	//private TextView       txtScanResult;
-	private EditText name;
+	// declare variables
 	byte[] byteArray;
 	Bitmap bp;
 	boolean notPhoto = false;
@@ -79,43 +69,34 @@ public class AddItemActivity extends BaseActivity  {
 	private Button cancelButton;
 	private ImageButton photoButton;
 	private Button ocrButton;
-	
-	
-	
+
 	Context context = this;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
+		// progress spinner
 		requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
+		// thread
         new Thread(new Runnable() { 
             public void run(){
                //All your heavy stuff here!!!
             }
         }).start();
 		
-
-        
-        
-        //FrameLayout linear = (FrameLayout) findViewById(R.id.content_frame);
-        //linear.setBackgroundResource(0);
         
 		super.onCreate(savedInstanceState);
 		
-		
+		// set the background to empty
 		LinearLayout  linearLayout = (LinearLayout) findViewById(R.id.linearLayoutid);
-		 linearLayout.setBackgroundResource(0);
+		linearLayout.setBackgroundResource(0);
 		
-		//requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
-		//getActionBar().setDisplayHomeAsUpEnabled(true);
-		
-		
-		//setContentView(R.layout.add_item);
+		// layout for list view
 		LayoutInflater inflater = (LayoutInflater) this
 	            .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 	    View contentView = inflater.inflate(R.layout.add_item, null, false);
 	    mDrawerLayout.addView(contentView, 0); 
 		
-	    
+	    // create an intent
 		Intent intent = this.getIntent();
 		
 		
@@ -125,45 +106,44 @@ public class AddItemActivity extends BaseActivity  {
 	    categoryEditText = (EditText) findViewById(R.id.itemCategory);
 	    publishEditText = (EditText) findViewById(R.id.publish_release_date);
 
-	    
+	    // setting imgview details
 	    imgView = (ImageView) findViewById(R.id.imageView1);
 	    int width = 260;
 	    int height = 326;
 	    LinearLayout.LayoutParams parms = new LinearLayout.LayoutParams(width,height);
 	    imgView.setLayoutParams(parms);
 	    
-	    
+	    // if received intent from another activity
 	    if(intent !=null)
         {
+	    	// which activity
 	        String strdata = intent.getExtras().getString("Uniqid");
 		    if(strdata.equals("from_Main"))
 	        {
-		    	
 				if (intent.getExtras() != null) {
-					
+					// item not found
 					if(intent.getExtras().containsKey("notFound")){
 						Toast.makeText(getApplicationContext(), "Item not found. Please enter it manually", Toast.LENGTH_SHORT).show();
 					}
 					else{
+					// get item details
 					String title = intent.getStringExtra("title");
 					String descritpion = intent.getStringExtra("description");
 					String category = intent.getStringExtra("category");
 					String image = intent.getStringExtra("image");
 					String author = intent.getStringExtra("author");
 					String date = intent.getStringExtra("release");
-
 					
+					// set details
 					titleEditText.setText(title);
 				    detailsEditText.setText(descritpion);
 				    categoryEditText.setText(category);
 				    authorEditText.setText(author);
 				    publishEditText.setText(date);
-
-				    
 				    itemImage = image;
 				    
+				    // set the image into a ParseFile and set imgview
 				    try{
-				    	
 				    	URL url = new URL(image);
 				    	HttpGet httpRequest = null;
 				    	httpRequest = new HttpGet(url.toURI());
@@ -186,10 +166,10 @@ public class AddItemActivity extends BaseActivity  {
 				        imgView.setImageBitmap(bitmap);
 				    	
 				    } catch (Exception ex){
-				    	
+				    	// Error
 				    }
 				}
-				}
+			  }
 	        }
 	        if(strdata.equals("from_OCR"))
 	        {
@@ -197,9 +177,11 @@ public class AddItemActivity extends BaseActivity  {
 	        	// Do Nothing
 	        	
 	        }
+	        // if item was click on in the list
 	        if(strdata.equals("from_List_Activity"))
 	        {
 	    	    if (intent.getExtras() != null) {
+	    	    	// get item details
 	    	        item = new ItemDetails(intent.getStringExtra("itemId"), intent.getStringExtra("itemTitle"), intent.getStringExtra("itemContent"), intent.getStringExtra("itemCategory"), intent.getStringExtra("itemAuthor_artist"), intent.getStringExtra("itemReleaseDate"));
 	    	        String imageUrl = intent.getStringExtra("itemImage");
 	    	        titleEditText.setText(item.getTitle());
@@ -207,7 +189,7 @@ public class AddItemActivity extends BaseActivity  {
 	    	        categoryEditText.setText(item.getCategory());
 	    	        authorEditText.setText(item.getAuthor_Artist());
 	    	        publishEditText.setText(item.getReleaseDate());
-	    	        
+	    	        // load img view from url
 	    	        Picasso.with(this).load(imageUrl).into(imgView);
 	    	    }
 	        }
@@ -217,29 +199,24 @@ public class AddItemActivity extends BaseActivity  {
 	        	// Do Nothing
 	        	
 	        }
-	        
         }
-	    
+	    // cancel button
 		cancelButton = (Button)findViewById(R.id.cancel_button);
 		cancelButton.setOnClickListener(new View.OnClickListener() {
 	        @Override
 	        public void onClick(View v) {
-	        	//Intent intent = new Intent(getApplicationContext(), ListItemActivity.class);
-	        	//startActivity(intent);
 	        	finish();
 	        }
 	    });
-		
-		
+		// save item button
 	    saveNoteButton = (Button)findViewById(R.id.save_button);
 	    saveNoteButton.setOnClickListener(new View.OnClickListener() {
 	        @Override
 	        public void onClick(View v) {
 	            saveItem();
-	            
 	        }
 	    });
-	    
+	    // open camera button
 	    photoButton = (ImageButton)findViewById(R.id.camera_button);
 	    photoButton.setOnClickListener(new View.OnClickListener() {
 	        @Override
@@ -248,12 +225,10 @@ public class AddItemActivity extends BaseActivity  {
 	        }
 	    });
 	    
-	    
+	    // which textbox was clicked
         titleEditText.setOnTouchListener(new View.OnTouchListener() {
-			
 			@Override
 			public boolean onTouch(View arg0, MotionEvent arg1) {
-				// TODO Auto-generated method stub
 				evalue = "1";
 				return false;
 			}
@@ -262,13 +237,12 @@ public class AddItemActivity extends BaseActivity  {
 			
 			@Override
 			public boolean onTouch(View arg0, MotionEvent arg1) {
-				// TODO Auto-generated method stub
 				evalue = "2";
 				return false;
 			}
 		});
        
-        
+        // put ocr text into whichever text box clicked on
         ocrButton = (Button)findViewById(R.id.ocr_button);
         ocrButton.setOnClickListener(new View.OnClickListener()
         {
@@ -276,14 +250,12 @@ public class AddItemActivity extends BaseActivity  {
                 if(evalue=="1")
                 {
                 	Intent intent = new Intent(context, com.example.myfirstapp.camerabase.CaptureActivity.class);
-
                 	startActivityForResult(intent, 1);
 
                 }
                 else if(evalue=="2")
                 {
                 	Intent intent = new Intent(context, com.example.myfirstapp.camerabase.CaptureActivity.class);
-
                 	startActivityForResult(intent, 2);
                 }
                 else{
@@ -291,112 +263,9 @@ public class AddItemActivity extends BaseActivity  {
                 }
             }
         });
-
-
-
-		
-	}
-	/*
-	private void saveItem(){
-		
-		itemTitle = titleEditText.getText().toString();
-        itemDetails = detailsEditText.getText().toString();
-        itemCategory = categoryEditText.getText().toString();
- 
-        itemTitle = itemTitle.trim();
-        itemDetails = itemDetails.trim();
-        itemCategory = itemCategory.trim();
- 
-        // If user doesn't enter a title or content, do nothing
-        // If user enters title, but no content, save
-        // If user enters content with no title, give warning
-        // If user enters both title and content, save
- 
-        if (!itemTitle.isEmpty()) {
- 
-            // Check if post is being created or edited
- 
-            if (item == null) {
-                // create new post
- 
-                ParseObject post = new ParseObject("Inventory");
-                post.put("title", itemTitle);
-                post.put("description", itemDetails);
-                post.put("category", itemCategory);
-                post.put("author", ParseUser.getCurrentUser());
-                setProgressBarIndeterminateVisibility(true);
-                post.saveEventually();
-                post.pinInBackground(new SaveCallback() {
-                    public void done(ParseException e) {
-                    	setProgressBarIndeterminateVisibility(false);
-                        if (e == null) {
-                            // Saved successfully.
-                            Toast.makeText(getApplicationContext(), "Saved", Toast.LENGTH_SHORT).show();
-                        	Intent intent = new Intent(getApplicationContext(), ListItemActivity.class);
-                        	startActivity(intent);
-                        } else {
-                            // The save failed.
-                            Toast.makeText(getApplicationContext(), "Failed to Save", Toast.LENGTH_SHORT).show();
-                            Log.d(getClass().getSimpleName(), "User update error: " + e);
-                        }
-                    }
-                });
- 
-            }
-            else {
-                // update post
- 
-                ParseQuery<ParseObject> query = ParseQuery.getQuery("Inventory");
-                query.fromLocalDatastore();
-                // Retrieve the object by id
-                query.getInBackground(item.getId(), new GetCallback<ParseObject>() {
-                  public void done(ParseObject post, ParseException e) {
-                    if (e == null) {
-                      // Now let's update it with some new data.
-                        post.put("title", itemTitle);
-                        post.put("description", itemDetails);
-                        post.put("category", itemCategory);
-                        post.put("author", ParseUser.getCurrentUser());
-                        setProgressBarIndeterminateVisibility(true);
-                        post.saveEventually();
-                        post.pinInBackground(new SaveCallback() {
-                            public void done(ParseException e) {
-                            	setProgressBarIndeterminateVisibility(false);
-                                if (e == null) {
-                                    // Saved successfully.
-                                    Toast.makeText(getApplicationContext(), "Saved", Toast.LENGTH_SHORT).show();
-                                	Intent intent = new Intent(getApplicationContext(), ListItemActivity.class);
-                                	startActivity(intent);
-                                } else {
-                                    // The save failed.
-                                    Toast.makeText(getApplicationContext(), "Failed to Save", Toast.LENGTH_SHORT).show();
-                                    Log.d(getClass().getSimpleName(), "User update error: " + e);
-                                }
-                            }
-                        });
-                    }
-                  }
-                });
-            }
-        }
-        else if (itemTitle.isEmpty() && !itemDetails.isEmpty() || itemTitle.isEmpty() && !itemCategory.isEmpty()) {
-            AlertDialog.Builder builder = new AlertDialog.Builder(AddItemActivity.this);
-            builder.setMessage(R.string.edit_error_message)
-                .setTitle(R.string.edit_error_title)
-                .setPositiveButton(android.R.string.ok, null);
-            AlertDialog dialog = builder.create();
-            dialog.show();
-        }
-
 	}
 	
-	*/
-	
-	/*
-	 * Old saveItem that only works with the cloud server
-	 */
-	
-	
+	// save the item to the cloud
 	private void saveItem() {
 		 
         itemTitle = titleEditText.getText().toString();
@@ -405,7 +274,6 @@ public class AddItemActivity extends BaseActivity  {
         itemAuthor_artist = authorEditText.getText().toString();
         itemReleaseDate = publishEditText.getText().toString();
         
-        //imageFile = new ParseFile("meal_photo.jpg", byteArray);
         itemTitle = itemTitle.trim();
         itemDetails = itemDetails.trim();
         itemCategory = itemCategory.trim();
@@ -419,10 +287,9 @@ public class AddItemActivity extends BaseActivity  {
  
         if (!itemTitle.isEmpty() && !itemCategory.isEmpty()) {
  
-            // Check if post is being created or edited
- 
-            if (item == null) {
-                // create new post
+            // Check if item is being created or edited
+             if (item == null) {
+                // create new item
  
                 ParseObject post = new ParseObject("Inventory");
                 itemTitle = itemTitle.toLowerCase();
@@ -430,7 +297,8 @@ public class AddItemActivity extends BaseActivity  {
                 itemCategory = itemCategory.toLowerCase();
                 itemAuthor_artist = itemAuthor_artist.toLowerCase();
                 itemReleaseDate = itemReleaseDate.toLowerCase();
-
+                
+                // save item details
                 post.put("title", itemTitle);
                 post.put("description", itemDetails);
                 post.put("category", itemCategory);
@@ -449,10 +317,8 @@ public class AddItemActivity extends BaseActivity  {
                 	}
                 	post.put("image", itemImage);
                     post.put("photo", imageFile);
-                    
-                    
                 }
-                
+                // else put blank image
                 else{
 			        ByteArrayOutputStream stream = new ByteArrayOutputStream();
 			        bp.compress(Bitmap.CompressFormat.PNG, 100, stream);
@@ -461,7 +327,7 @@ public class AddItemActivity extends BaseActivity  {
                     post.put("photo", imageFile);
                     notPhoto = false;
                 }
-                
+                // save for current user
                 post.put("author", ParseUser.getCurrentUser());
                 setProgressBarIndeterminateVisibility(true);
                 post.saveInBackground(new SaveCallback() {
@@ -481,8 +347,8 @@ public class AddItemActivity extends BaseActivity  {
  
             }
             else {
-                // update post
- 
+                // update item
+            	// select from database
                 ParseQuery<ParseObject> query = ParseQuery.getQuery("Inventory");
  
                 // Retrieve the object by id
@@ -533,7 +399,8 @@ public class AddItemActivity extends BaseActivity  {
                 });
             }
         }
-        else if (itemTitle.isEmpty() || itemCategory.isEmpty()) { // || itemTitle.isEmpty() && !itemCategory.isEmpty()
+        // validation for user not entering data and pressing save
+        else if (itemTitle.isEmpty() || itemCategory.isEmpty()) {
         	String alertHeader = "Please Enter: ";
         	String alertTitle = "Title";
         	String alertCate = "Category";
@@ -551,40 +418,31 @@ public class AddItemActivity extends BaseActivity  {
 	public void openCamera(){
 	      Intent intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
 	      startActivityForResult(intent, 0);
-	      
-	      
 	}
 
+		// check which activity returned a result and get the data
 		protected void onActivityResult(int requestCode, int resultCode, Intent data){
 		    switch(requestCode){
-		        case 0: // Do your stuff here...
+		        case 0:
 		        	if(resultCode != RESULT_CANCELED){
-		  	      // TODO Auto-generated method stub
 		  	      super.onActivityResult(requestCode, resultCode, data);
 		  	      notPhoto = true;
 		  	      bp = (Bitmap) data.getExtras().get("data");
 		  	      imgView.setImageBitmap(bp);
 		        	}
 		        break;
-		        case 1: // Do your other stuff here...
+		        case 1:
 		        	if(data!=null){
-			        	//System.out.println("got here");
 	                    ocrText = data.getStringExtra("ocr");
 	                    titleEditText.setText(ocrText);
                     }
-		        	
 		        break;
-		        case 2: // Do your other stuff here...
+		        case 2:
 		        	if(data != null){
                     ocrText = data.getStringExtra("ocr");
                     detailsEditText.setText(ocrText);
 		        	}
 		        break;
-		        
 		    }
 		}
-	
-	
-	
-
-}
+	}
